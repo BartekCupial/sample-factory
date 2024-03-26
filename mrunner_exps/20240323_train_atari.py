5a76beccbd87f57419b2cfc0cbd9aa45412ecef1
Bartłomiej Cupiał <bartlomiej.cupial@gmail.com>
2024-03-26 19:29:03 +0100

    experiments on 4 games atari

--- mrunner_exps/20240323_train_atari.py
+++ mrunner_exps/20240323_train_atari.py
@@ -6,27 +6,21 @@ name = globals()["script"][:-3]
 config = {
     "env": "atari_breakout",
     "exp_tags": [name],
-
-    "train_for_env_steps": 2_000_000_000,
+    "train_for_env_steps": 5_000_000,
     "group": "monk-APPO-KLAA-T",
-
-    "num_workers": 4,
+    "num_workers": 8,
     "num_envs_per_worker": 16,
     "worker_num_splits": 2,
     "rollout": 32,
     "async_rl": True,
     "restart_behavior": "overwrite",
-
     "save_milestones_ith": 10_000_000,
-
     # Wandb settings
-    "wandb_user": "rahid",
-    "wandb_project": "atari_sf",
-    "wandb_group": "rahid",
-    "wandb_tags": [name],
-
-    "batch_size": 32,
-    "dataset_batch_size": 128,  # this equals bs = 512, 512 * 32 = 16384
+    "wandb_user": "bartekcupial",
+    "wandb_project": "sf2_nethack",
+    "wandb_group": "gmum",
+    "batch_size": 256,
+    "dataset_batch_size": 512,  # this equals bs = 512, 512 * 32 = 16384
     "with_wandb": True,
     "serial_mode": False,
 }
--- mrunner_exps/20240326_atari_finetune.py
+++ mrunner_exps/20240326_atari_finetune.py
@@ -6,36 +6,37 @@ name = globals()["script"][:-3]
 config = {
     "env": "atari_breakout",
     "exp_tags": [name],
-
     "train_for_env_steps": 2_000_000_000,
     "group": "monk-APPO-KLAA-T",
-
-    "num_workers": 4,
+    "num_workers": 8,
     "num_envs_per_worker": 16,
     "worker_num_splits": 2,
     "rollout": 32,
     "async_rl": True,
     "restart_behavior": "overwrite",
-
     "save_milestones_ith": 10_000_000,
-
     # Wandb settings
-    "wandb_user": "rahid",
-    "wandb_project": "atari_sf",
-    "wandb_group": "rahid",
+    "wandb_user": "bartekcupial",
+    "wandb_project": "sf2_atari",
+    "wandb_group": "gmum",
     "wandb_tags": [name],
-
-    "batch_size": 32,
-    "dataset_batch_size": 128,  # this equals bs = 512, 512 * 32 = 16384
+    "batch_size": 256,
+    "dataset_batch_size": 512,  # this equals bs = 512, 512 * 32 = 16384
     "with_wandb": True,
     "serial_mode": False,
-
     "use_pretrained_checkpoint": True,
-    "model_path": "/home/maciejwolczyk/breakout_checkpoint/default_experiment/",
+    # "model_path": "/home/maciejwolczyk/breakout_checkpoint/default_experiment/",
+    "model_path": "train_dir/breakout/default_experiment",
     "kickstarting_loss_coeff": 0.0,
     "skip_train": 5_000_000,
-    "freeze": {"encoder": 0, "core": 0, "decoder": 0, "action_parameterization": 0},
-    "unfreeze": {"encoder": 10_000_000, "core": 10_000_000, "decoder": 10_000_000, "action_parameterization": 10_000_000} ,
+    "freeze": {"actor_encoder": 0, "actor_core": 0, "actor_decoder": 0, "action_parameterization": 0},
+    "unfreeze": {
+        "actor_encoder": 10_000_000,
+        "actor_core": 10_000_000,
+        "actor_decoder": 10_000_000,
+        "action_parameterization": 10_000_000,
+    },
+    "actor_critic_share_weights": False,
 }
 
 atari_games = ["breakout", "qbert", "montezuma", "upndown"]
@@ -48,7 +49,8 @@ for atari_game in atari_games:
         {
             "seed": list(range(1)),
             "learning_rate": [1e-4, 1e-3],
-            "value_loss_coeff": [1e-2, 1e-1],
+            # "value_loss_coeff": [1e-2, 1e-1],
+            "reward_scale": [1e-2, 1e-1],
             "model_path": [f"/atari_checkpoints/{atari_game}/default_experiment/"],
             "env": [f"atari_{atari_game}"],
         },
