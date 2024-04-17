ac873c37c4cc2329016c536adcef1b664c91dd80
Ewa Dobrowolska <ed429245@students.mimuw.edu.pl>
2024-04-17 12:23:58 +0200

    exp unfreeze modules update

--- mrunner_exps/modules_unfreeze_ks.py
+++ mrunner_exps/modules_unfreeze_ks.py
@@ -11,7 +11,7 @@ config = {
     "group": "monk-APPO-KS-T",
     "character": "mon-hum-neu-mal",
     "num_workers": 16,
-    "num_envs_per_worker": 16,
+    "num_envs_per_worker": 32,
     "worker_num_splits": 2,
     "rollout": 32,
     "batch_size": 4096,  # this equals bs = 128, 128 * 32 = 4096
@@ -19,9 +19,11 @@ config = {
     "serial_mode": False,
     "wandb_user": "e-dobrowolska",
     "wandb_project": "nethack",
-    "wandb_group": "ks_modules_unfreeze",
+    "wandb_group": "ks_lora",
     "with_wandb": True,
-    "kickstarting_loss_coeff": 0.5,
+    "use_pretrained_checkpoint": True,
+    "model_path": "/net/pr2/projects/plgrid/plgggmum_crl/bcupial/sf_checkpoints/amzn-AA-BC_pretrained",
+    "kickstarting_loss_coeff": 0.75,
     "kickstarting_loss_decay": 0.99998,
     "teacher_path": "/net/pr2/projects/plgrid/plgggmum_crl/bcupial/sf_checkpoints/amzn-AA-BC_pretrained",
     "run_teacher_hs": False,
@@ -34,34 +36,18 @@ config = {
     "h_dim": 1738,
     "gamma": 1.0,
     "skip_train": 25_000_000,
-    "use_pretrained_checkpoint": True,
-    "model_path": "/net/pr2/projects/plgrid/plgggmum_crl/bcupial/sf_checkpoints/amzn-AA-BC_pretrained",
     "lr_schedule": "linear_decay",
-    "heartbeat_interval": 600,
-    "heartbeat_reporting_interval": 1200,
 }
 
-expected_batch_size = 4096
-rollout = 256
-target_batch_size = 256
-
-batch_size = min(expected_batch_size, min(target_batch_size * rollout, expected_batch_size * 8))
-batches_to_accumulate = max(1, (rollout * target_batch_size) // expected_batch_size)
-optim_step_every_ith = max(1, batches_to_accumulate // 8)
 params_grid = [
     {
         "seed": list(range(3)),
-        "learning_rate": [0.0001],
         "freeze": [{"encoder": 0}],
         "modules_unfreeze": [{"encoder": ["fc"]}],
-        "rollout": [rollout],
-        "batch_size": [batch_size],  # 32 * 512, 64 * 256, 128 * 128
-        "num_batches_per_epoch": [min(8, batches_to_accumulate)],
-        "optim_step_every_ith": [optim_step_every_ith],
-        "target_batch_size": [target_batch_size],
         "kickstarting_loss_decay": [0.9999, 0.99996, 0.99998],
-    }
+    },
 ]
+
 
 experiments_list = create_experiments_helper(
     experiment_name=name,
